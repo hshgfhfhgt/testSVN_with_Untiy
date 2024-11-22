3bd9f4646b012e0391b05c89fb8b30f54ae84751
hshgfhfhgt <hansebzwd@naver.com>
2024-11-22 13:12:56 +0900

    A 스크립트 내용을 변경

--- Assets/A.cs
+++ Assets/A.cs
@@ -7,7 +7,7 @@ public class A : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("A 스크립트 내용을 변경했어요.");
     }
 
     // Update is called once per frame
